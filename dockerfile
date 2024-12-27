89ca6e21abe1277605f772229fe631988a89c6d7
agasurfer <agasurfer@hotmail.com>
2024-12-27 14:40:51 +0100

    attempt2

--- dockerfile
+++ dockerfile
@@ -5,4 +5,5 @@ RUN npm install
 COPY . .
 RUN npm run build
 EXPOSE 80
+HEALTHCHECK --interval=30s --timeout=30s --start-period=5s --retries=3 CMD curl -f http://localhost:80 || exit 1
 CMD ["npm", "run", "preview"]
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
-/* import { defineConfig } from 'vite'
+ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+/* // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-}) */
+}) * */
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
+    host: '0.0.0.0',
     port: 80
   },
   preview: {
-    host: true,
+    host: '0.0.0.0',
     port: 80
   }
 })
